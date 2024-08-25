67fe2aacc499c3fe6d5ac94065d6d91c390f3af3
LDY <leedy3838@naver.com>
2024-08-25 13:42:17 +0900

    feat: MongoDB 설정

---
+++ src/main/java/com/shwimping/be/place/domain/mongodb/MongoPlace.java
@@ -0,0 +1,40 @@
+package com.shwimping.be.place.domain.mongodb;
+
+import com.shwimping.be.place.domain.type.Category;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "place")
+public class MongoPlace {
+
+    @MongoId
+    @Field(name = "_id")
+    private String id;
+
+    @Field(name = "category")
+    private Category category;
+
+    @Field(name = "address")
+    private String address;
+
+    @Field(name = "location")
+    @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+    private GeoJsonPoint location;
+
+    @Builder
+    public MongoPlace(Category category, String address, GeoJsonPoint location) {
+        this.category = category;
+        this.address = address;
+        this.location = location;
+    }
+}
