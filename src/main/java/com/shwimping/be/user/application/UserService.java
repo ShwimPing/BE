67a638a59f01aa84d367880be795b6928bddbe4d
Amepistheo <kellylhji@gmail.com>
2024-08-26 22:51:49 +0900

    feat: 자체 회원가입 비즈니스 로직 1차 구현 #1

---
+++ src/main/java/com/shwimping/be/user/application/UserService.java
@@ -0,0 +1,24 @@
+package com.shwimping.be.user.application;
+
+import com.shwimping.be.user.domain.User;
+import com.shwimping.be.user.dto.request.CreateUserRequest;
+import com.shwimping.be.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void createUser(CreateUserRequest request) {
+        User user = request.toUser();
+        userRepository.save(user);
+    }
+}
---
+++ src/main/java/com/shwimping/be/user/dto/request/CreateUserRequest.java
@@ -0,0 +1,26 @@
+package com.shwimping.be.user.dto.request;
+
+import com.shwimping.be.user.domain.User;
+import jakarta.validation.constraints.Email;
+
+import static com.shwimping.be.user.domain.type.Provider.SELF;
+
+public record CreateUserRequest(
+    @Email(message = "이메일 형식을 맞춰주세요")
+    String email,
+    String password,
+    String fcmToken
+) {
+    public User toUser() {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .fcmToken(fcmToken)
+                .provider(SELF)
+                .nickname("temporal")
+                .isAlarmAllowed(true)
+                .profileImageUrl("temporal.png")
+                .nowLocation("temporal")
+                .build();
+    }
+}
