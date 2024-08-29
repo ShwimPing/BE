b1d3e3842623f27d233ea4355ab8936482bd8431
Amepistheo <kellylhji@gmail.com>
2024-08-29 16:58:22 +0900

    remove: 사용하지 않는 코드 정리 #1

--- src/main/java/com/shwimping/be/user/presentation/UserController.java
+++ src/main/java/com/shwimping/be/user/presentation/UserController.java
@@ -1,20 +1,11 @@
 package com.shwimping.be.user.presentation;
 
-import com.shwimping.be.global.dto.ResponseTemplate;
 import com.shwimping.be.user.application.UserService;
-import com.shwimping.be.user.dto.request.CreateUserRequest;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.shwimping.be.global.dto.ResponseTemplate.EMPTY_RESPONSE;
 
 @Tag(name = "User", description = "유저 관련 API")
 @Slf4j
@@ -24,14 +15,4 @@ import static com.shwimping.be.global.dto.ResponseTemplate.EMPTY_RESPONSE;
 public class UserController {
 
     private final UserService userService;
-
-    @Operation(summary = "자체 회원가입", description = "자체 로그인에서 회원가입하기")
-    @PostMapping("/signup")
-    public ResponseEntity<ResponseTemplate<Object>> createUser(@RequestBody CreateUserRequest request) {
-        userService.createUser(request);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(EMPTY_RESPONSE);
-    }
 }
