a14bfbd3cd93d1635f3774afc09b6d616bae00e3
leedy3838 <leedy3838@naver.com>
2024-09-01 21:55:12 +0900

    feat: jar 파일에서도 csv 파일을 읽어 오기 위한 수정

--- src/main/java/com/shwimping/be/place/repository/init/PlaceInitializer.java
+++ src/main/java/com/shwimping/be/place/repository/init/PlaceInitializer.java
@@ -8,6 +8,7 @@ import com.shwimping.be.place.domain.type.Category;
 import com.shwimping.be.place.repository.PlaceRepository;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
@@ -33,22 +34,19 @@ public class PlaceInitializer implements ApplicationRunner {
         if (placeRepository.count() > 0) {
             log.info("[Place]더미 데이터 존재");
         } else {
-            importCsvToPlace("src/main/resources/data/기후동행쉼터.csv",
-                    4, 9, 10, Category.TOGETHER, 2, 3, 7, 8);
-            importCsvToPlace("src/main/resources/data/도서관 쉼터.csv",
-                    6, 7, 8, Category.LIBRARY, 2, 1, 3, 4);
-            importCsvToPlace("src/main/resources/data/서울시 무더위쉼터.csv",
-                    3, 7, 8, Category.HOT, 2, 11, Integer.MAX_VALUE, Integer.MAX_VALUE);
-            importCsvToPlace("src/main/resources/data/서울시 한파쉼터.csv",
-                    2, 9, 10, Category.COLD, 1, 14, 7, 8);
-            importCsvToPlace("src/main/resources/data/스마트쉼터 현황.csv",
-                    4, 6, 7, Category.SMART, 2, 1, Integer.MAX_VALUE, Integer.MAX_VALUE);
+            importCsvToPlace("data/기후동행쉼터.csv", 4, 9, 10, Category.TOGETHER, 2, 3, 7, 8);
+            importCsvToPlace("data/도서관 쉼터.csv", 6, 7, 8, Category.LIBRARY, 2, 1, 3, 4);
+            importCsvToPlace("data/서울시 무더위쉼터.csv", 3, 7, 8, Category.HOT, 2, 11, Integer.MAX_VALUE, Integer.MAX_VALUE);
+            importCsvToPlace("data/서울시 한파쉼터.csv", 2, 9, 10, Category.COLD, 1, 14, 7, 8);
+            importCsvToPlace("data/스마트쉼터 현황.csv", 4, 6, 7, Category.SMART, 2, 1, Integer.MAX_VALUE, Integer.MAX_VALUE);
         }
     }
 
     private void importCsvToPlace(String filePath, int addressIdx, int longitudeIdx, int latitudeIdx, Category category,
                                   int nameIdx, int regionIdx, int openTimeIdx, int closeTimeIdx) {
-        try (Reader reader = new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8)) {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePath);
+             Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        ) {
             CSVReader csvReader = new CSVReader(reader);
             String[] nextLine;
             // 첫 번째 줄은 헤더이므로 건너뜁니다.
@@ -60,7 +58,8 @@ public class PlaceInitializer implements ApplicationRunner {
                 double longitude = Double.parseDouble(nextLine[longitudeIdx]); // 경도
                 String name = nextLine[nameIdx];
                 String region = nextLine[regionIdx];
-                LocalTime openTime = nextLine.length > openTimeIdx ? LocalTime.parse(nextLine[openTimeIdx]) : LocalTime.of(0, 0);
+                LocalTime openTime =
+                        nextLine.length > openTimeIdx ? LocalTime.parse(nextLine[openTimeIdx]) : LocalTime.of(0, 0);
 
                 String closeTimeString = nextLine.length > closeTimeIdx ? nextLine[closeTimeIdx] : "00:00:00";
                 if ("24:00:00".equals(closeTimeString) || "00:00:00".equals(closeTimeString)) {
