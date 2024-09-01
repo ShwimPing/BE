5cd9b6d73faa21ce399db8c8f54b81324e35f149
leedy3838 <leedy3838@naver.com>
2024-09-01 22:10:52 +0900

    feat: jswagger에 예시 데이터 설명 추가

--- src/main/java/com/shwimping/be/place/presentation/PlaceController.java
+++ src/main/java/com/shwimping/be/place/presentation/PlaceController.java
@@ -23,7 +23,7 @@ public class PlaceController {
 
     private final PlaceService placeService;
 
-    @Operation(summary = "주변 장소 검색", description = "경도, 위도, 거리를 기반으로 검색, 거리는 m 단위로 입력")
+    @Operation(summary = "주변 장소 검색", description = "경도, 위도, 거리를 기반으로 검색, 거리는 m 단위로 입력 - 예시 데이터 경도: 127.0965824, 위도: 37.47153792 - 서울특별시 강남구 자곡로 116(도서관 쉼터)")
     @GetMapping("/nearby")
     public ResponseEntity<ResponseTemplate<?>> getNearbyPlaces(
             @RequestParam double longitude,
