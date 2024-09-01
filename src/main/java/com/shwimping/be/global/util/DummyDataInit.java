79e4aca7eb436e6cce9eae54aab709e6fcf55754
leedy3838 <leedy3838@naver.com>
2024-09-01 18:04:40 +0900

    feat: 더미 데이터 설정 변경

--- src/main/java/com/shwimping/be/global/util/DummyDataInit.java
+++ src/main/java/com/shwimping/be/global/util/DummyDataInit.java
@@ -8,12 +8,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * 로컬 환경에서 더미 데이터를 넣을 때 사용하는 annotation Profile이 dev인 경우 사용 일단 현재 개발단계여서 그냥 더미 데이터로 쓰려고 다 열어 놓음. - 나중에 배포 시 닫기
- */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Profile({"local", "dev"})
+@Profile({"local", "dev", "prod"})
 @Transactional
 @Component
 public @interface DummyDataInit {
