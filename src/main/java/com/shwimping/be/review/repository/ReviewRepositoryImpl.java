679a256cf64cbc93938f4fbe882404f1ee69c1ae
Amepistheo <kellylhji@gmail.com>
2024-09-24 00:46:53 +0900

    feat: 내 리뷰 모아보기 기능 구현 #29

--- src/main/java/com/shwimping/be/review/repository/ReviewRepositoryImpl.java
+++ src/main/java/com/shwimping/be/review/repository/ReviewRepositoryImpl.java
@@ -35,10 +35,56 @@ public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
     }
 
     @Override
+    public List<ReviewSimpleResponse> getMyFirstReview(Long userId) {
+        // 리뷰를 최신 5개만 가져오기 위한 서브쿼리
+        return jpaQueryFactory.select(Projections.constructor(ReviewSimpleResponse.class,
+                        review.id,
+                        review.user.nickname,
+                        review.content,
+                        review.rating,
+                        review.date,
+                        review.reviewImageUrl
+                ))
+                .from(review)
+                .leftJoin(review.user)
+                .where(review.user.id.eq(userId))
+                .orderBy(review.date.desc())
+                .limit(5)
+                .fetch();
+    }
+
+    @Override
+    public List<ReviewSimpleResponse> getMyReview(Long userId, Long lastReviewId, Long size) {
+        return jpaQueryFactory.select(
+                        Projections.constructor(ReviewSimpleResponse.class,
+                                review.id,
+                                review.user.nickname,
+                                review.content,
+                                review.rating,
+                                review.date,
+                                review.reviewImageUrl
+                        )
+                )
+                .from(review)
+                .where(review.user.id.eq(userId), review.id.lt(lastReviewId)) // lastReviewId를 기준으로 필터링
+                .orderBy(review.id.desc())
+                .limit(size) // 가져올 리뷰 수 제한
+                .fetch();
+    }
+
+    @Override
     public Boolean hasNext(Long placeId, Long lastReviewId, Long size) {
         return jpaQueryFactory.selectOne()
                 .from(review)
                 .where(review.place.id.eq(placeId), review.id.lt(lastReviewId - size))
+                .fetchFirst() != null;
+    }
+
+    @Override
+    public Boolean hasNextMyReview(Long userId, Long lastReviewId, Long size) {
+        return jpaQueryFactory.selectOne()
+                .from(review)
+                .where(review.user.id.eq(userId), review.id.lt(lastReviewId - size))
                 .fetchFirst() != null;
     }
 }
--- src/main/java/com/shwimping/be/user/presentation/UserController.java
+++ src/main/java/com/shwimping/be/user/presentation/UserController.java
@@ -1,6 +1,7 @@
 package com.shwimping.be.user.presentation;
 
 import com.shwimping.be.global.dto.ResponseTemplate;
+import com.shwimping.be.review.application.ReviewService;
 import com.shwimping.be.user.application.UserService;
 import com.shwimping.be.user.dto.request.UpdateProfileRequest;
 import com.shwimping.be.user.dto.response.MypageResponse;
@@ -26,6 +27,7 @@ import static com.shwimping.be.global.dto.ResponseTemplate.EMPTY_RESPONSE;
 public class UserController {
 
     private final UserService userService;
+    private final ReviewService reviewService;
 
     @Operation(summary = "마이페이지 조회", description = "마이페이지 조회")
     @GetMapping
@@ -51,6 +53,19 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(EMPTY_RESPONSE);
+    }
+
+    @Operation(summary = "내 리뷰 모아보기", description = "리뷰 처음 조회 시 lastReviewId에 0을 넣어주세요<br>" +
+            "리뷰는 최신순으로 제공됩니다")
+    @GetMapping("/review")
+    public ResponseEntity<ResponseTemplate<?>> getMyReview(
+            @AuthenticationPrincipal Long userId,
+            @RequestParam Long lastReviewId,
+            @RequestParam(defaultValue = "5") Long size) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseTemplate.from(reviewService.getMyReview(userId, lastReviewId, size)));
     }
 
     @Operation(summary = "푸시 알림 설정", description = "푸시 알림 on/off 설정 기능")
